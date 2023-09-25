b256a584328fc8b0956ed2e920334cee207d9c54
Dominic Salmieri <dsalmieri@olin.edu>
2023-09-25 16:05:46 -0400

    Added pan/tilt movement

--- miniproject2/miniproject2.ino
+++ miniproject2/miniproject2.ino
@@ -20,68 +20,71 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogInOutSerial
 */
 
-// These constants won't change. They're used to give names to the pins used:
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int analogOutPin = 9;  // Analog output pin that the LED is attached to
+#include <Servo.h>
 
 int sensorValue = 0;  // value read from the pot
 int outputValue = 0;  // value output to the PWM (analog out)
 
-#include <Servo.h>
+int sensor_pin = A0;
+int tilt_pin = 10;
+int pan_pin = 11;
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+int min_tilt = 75;
+int max_tilt = 150;
 
-int pos = 0;    // variable to store the servo position
+int min_pan = 45;
+int max_pan = 135;
 
-// Create variables necessary for timing components. These variables are unsigned long types becuase they are going to become very large very quickly. 
-unsigned long previousMillis = 0;  // Create the previousMillis variable that will store last time LED was updated. Set it intially to 0.
-unsigned long previousMillis1 = 0;  // Create the previousMillis1 variable that will store last time LED was updated. Set it intially to 0.
-//const long interval = 3000;  // Create the interval variable. This will the interval at which the LED blinks (milliseconds).
-//const long interval2 = 250; // Create the interval2 variable. This will the interval at which the LED blinks (milliseconds). This interval specifies a shorter time than the one above.
+Servo tilt_servo;  // create servo object to control a servo
+Servo pan_servo;
 
 void setup() {
   // initialize serial communications at 9600 bps:
   // start the serial port
   long baudRate = 9600;       // NOTE1: The baudRate for sending & receiving programs must match
   Serial.begin(baudRate);     // NOTE2: Set the baudRate to 115200 for faster communication
-  myservo.attach(11);          // attaches the servo on pin 11 to the servo object
+  tilt_servo.attach(tilt_pin);          // attaches the servo on pin 11 to the servo object
+  pan_servo.attach(pan_pin); 
+}
+
+int single_scan(int angle) {
+  pan_servo.write(angle);              // tell servo to go to position in variable 'pos'
+  delay(500);
+
+  int min = 1000;
+
+  for (int i = 0; i < 100; i++) {
+    int sensorValue = analogRead(sensor_pin);
+    if (sensorValue < min) {
+      min = sensorValue;
+    }
+  }
+
+  int outputValue = map(min, 0, 1023, 0, 255);
+
+  return outputValue;
 }
 
 void loop() {
-    // servo sweep 
-      for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-          // in steps of 1 degree
-          myservo.write(pos);              // tell servo to go to position in variable 'pos'
+  for (int tilt = max_tilt; tilt >= min_tilt; tilt -= 4) {
+    tilt_servo.write(tilt);
+    delay(500);
 
-          delay(500);
+    for (int pan = min_pan; pan <= max_pan; pan += 2) { // goes from 0 degrees to 180 degrees
+      outputValue = single_scan(pan);
 
-          // read the analog in value:
-          sensorValue = analogRead(analogInPin);
-          // map it to the range of the analog out:
-          outputValue = map(sensorValue, 0, 1023, 0, 255);
-          // change the analog out value:
-          analogWrite(analogOutPin, outputValue);
+      // print the results to the Serial Monitor:
+      Serial.println(outputValue);
+    }
 
-          // print the results to the Serial Monitor:
-          //Serial.print("output = ");
-          Serial.println(outputValue);
-        }
+    tilt_servo.write(tilt-2);
+    delay(500);
 
-      for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-          myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    for (int pan = max_pan; pan >= min_pan; pan -= 2) { // goes from 180 degrees to 0 degrees
+      outputValue = single_scan(pan);
 
-          delay(500);
-
-          // read the analog in value:
-          sensorValue = analogRead(analogInPin);
-          // map it to the range of the analog out:
-          outputValue = map(sensorValue, 0, 1023, 0, 255);
-          // change the analog out value:
-          analogWrite(analogOutPin, outputValue);
-
-          // print the results to the Serial Monitor:
-          //Serial.print("output = ");
-          Serial.println(outputValue);      
-        }
-  }    
+      // print the results to the Serial Monitor:
+      Serial.println(outputValue);
+    }
+  }
+}    
---
+++ transform_scan.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+def transform_scan(distance: float, tilt_angle: float, pan_angle: float):
+    # Create a point the measured distance away from the sensor in the sensors frame
+    point = np.array([distance, 0, 0, 1])
+
+    distance_to_tilt_servo = np.array([
+        [1, 0, 0, 0.75],
+        [0, 1, 0,    0],
+        [0, 0, 1,    0],
+        [0, 0, 0,    1]
+        ])
+    point = np.dot(distance_to_tilt_servo, point)
+
+    tilt_servo_rotation = np.array([
+        [np.cos(tilt_angle), 0, -np.sin(tilt_angle), 0],
+        [                 0, 1,                   0, 0],
+        [np.sin(tilt_angle), 0,  np.cos(tilt_angle), 0],
+        [                 0, 0,                   0, 1]
+        ])
+    point = np.dot(tilt_servo_rotation, point)
+
+    tilt_servo_to_pan_servo = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, -2.25],
+        [0, 0, 0, 1]
+        ])
+    point = np.dot(tilt_servo_to_pan_servo, point)
+
+    pan_servo_rotation = np.array([
+        [np.cos(pan_angle), -np.sin(pan_angle), 0, 0],
+        [np.sin(pan_angle),  np.cos(pan_angle), 0, 0],
+        [                 0,                 0, 1, 0],
+        [                 0,                 0, 0, 1]
+        ])
+    point = np.dot(pan_servo_rotation, point)
+
+    pan_servo_to_ground = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, -2.25],
+        [0, 0, 0, 1]
+        ])
+    point = np.dot(pan_servo_to_ground, point)
+
+    x, y, z = point[0:3]
+    return (x, y, z)
+
+print(transform_scan(1, 1, 1))
