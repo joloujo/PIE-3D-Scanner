ba55e13d8632111078626800660c0cfdb342f93c
Dominic Salmieri <dsalmieri@olin.edu>
2023-09-27 23:12:53 -0400

    comments and calibration curve

--- transform_scan.py
+++ transform_scan.py
@@ -1,10 +1,11 @@
+# import necesary libraries
 import numpy as np
 
 def transform_scan(distance: float, tilt_angle: float, pan_angle: float):
     # Create a point the measured distance away from the sensor in the sensors frame
     point = np.array([distance, 0, 0, 1])
 
-    # transform from the sensor to the 
+    # transform from the sensor frame to the tilt servo frame 
     distance_to_tilt_servo = np.array([
         [1, 0, 0, 0.75],
         [0, 1, 0,    0],
@@ -13,6 +14,7 @@ def transform_scan(distance: float, tilt_angle: float, pan_angle: float):
         ])
     point = np.dot(distance_to_tilt_servo, point)
 
+    # rotate the point by the tilt servo angle 
     tilt_servo_rotation = np.array([
         [np.cos(tilt_angle), 0, -np.sin(tilt_angle), 0],
         [                 0, 1,                   0, 0],
@@ -21,6 +23,7 @@ def transform_scan(distance: float, tilt_angle: float, pan_angle: float):
         ])
     point = np.dot(tilt_servo_rotation, point)
 
+    # transform from the tilt servo frame to the pan servo frame 
     tilt_servo_to_pan_servo = np.array([
         [1, 0, 0, 0],
         [0, 1, 0, 0],
@@ -29,6 +32,7 @@ def transform_scan(distance: float, tilt_angle: float, pan_angle: float):
         ])
     point = np.dot(tilt_servo_to_pan_servo, point)
 
+    # rotate the point by the pan servo angle 
     pan_servo_rotation = np.array([
         [np.cos(pan_angle), -np.sin(pan_angle), 0, 0],
         [np.sin(pan_angle),  np.cos(pan_angle), 0, 0],
@@ -37,6 +41,7 @@ def transform_scan(distance: float, tilt_angle: float, pan_angle: float):
         ])
     point = np.dot(pan_servo_rotation, point)
 
+    # transform from the pan servo to the ground frame 
     pan_servo_to_ground = np.array([
         [1, 0, 0, 0],
         [0, 1, 0, 0],
